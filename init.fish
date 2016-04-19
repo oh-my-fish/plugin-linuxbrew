2699274f35802132ec94480cc132e6ab1fe0722d
Andrew Rynhard <rynhard.a@icloud.com>
2016-04-18 21:36:48 -0700

    Add sbin path to satisfy brew doctor

--- init.fish
+++ init.fish
@@ -1,9 +1,13 @@
-set -l linuxbrew_path "$HOME/.linuxbrew/bin"
+set -l linuxbrew_bin_path "$HOME/.linuxbrew/bin"
+set -l linuxbrew_sbin_path "$HOME/.linuxbrew/sbin"
 set -l linuxbrew_manpath "$HOME/.linuxbrew/share/man"
 set -l linuxbrew_infopath "$HOME/.linuxbrew/share/info"
 
-contains -- $linuxbrew_path $PATH
-  or set -gx PATH $linuxbrew_path $PATH
+contains -- $linuxbrew_bin_path $PATH
+  or set -gx PATH $linuxbrew_bin_path $PATH
+
+contains -- $linuxbrew_sbin_path $PATH
+  or set -gx PATH $linuxbrew_sbin_path $PATH
 
 contains -- $linuxbrew_manpath $MANPATH
   or set -gx MANPATH $linuxbrew_manpath $MANPATH
